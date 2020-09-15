d3035bd9a533ca6a5cb24a48925d782e7c667eff
sconnolly5 <sconnolly5@uclan.ac.uk>
2020-09-15 18:05:02 +0100

    Create and join buttons linked up.
    Create and join buttons are now linked up.

--- js/lobbies.js
+++ js/lobbies.js
@@ -1,10 +1,20 @@
 const API_PROTOCOL = "http://"
 const API_URL = "localhost";
 const API_PORT = 8080;
-const GET_LOBBIES_URL = "/lobbies"
+const GET_LOBBIES_URL = "/lobbies/"
+const CREATE_LOBBY_URL = GET_LOBBIES_URL + "/create"
 
 $(document).ready(function() {
-    $('#joinLobbyForm').submit(event => {
+    initButtonJoinHandler();
+    initButtonCreateHandler();
+});
+
+function initButtonJoinHandler() {
+    var btnJoin = $('#joinLobbyForm');
+    if (isNull(btnJoin)) {
+        return;
+    }
+    btnJoin.submit(event => {
         var url = API_PROTOCOL + API_URL + ":" + API_PORT + GET_LOBBIES_URL;
         $.getJSON(url, function(data) {
             found = false;
@@ -16,8 +26,27 @@ $(document).ready(function() {
             found ? console.log("Found") : console.log("Not found");
         });
 
-
         // Stop redirection
         event.preventDefault();
     });
-});
+}
+
+function initButtonCreateHandler() {
+    var buttonCreate = $('#btnCreateLobby');
+    if (isNull(buttonCreate)) {
+        return;
+    }
+    buttonCreate.click(function(event) {
+        var url = API_PROTOCOL + API_URL + ":" + API_PORT + CREATE_LOBBY_URL;
+
+        $.getJSON(url, function(data) {
+            found = false;
+            console.log("Room code is: " + data.name);
+        });
+        event.preventDefault();
+    });
+}
+
+function isNull(obj) {
+    return obj == 'undefined' || obj == null;
+}
