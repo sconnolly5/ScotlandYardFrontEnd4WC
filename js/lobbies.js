cd25c88a6ff64dd2957daee0600cdba617644983
sconnolly5 <sconnolly5@uclan.ac.uk>
2020-09-16 09:32:14 +0100

    Added a handful of new functions, ability to get and set cookies.
    Who pushes changes they haven't properly tested? I DO.

--- js/lobbies.js
+++ js/lobbies.js
@@ -3,10 +3,13 @@ const API_URL = "localhost";
 const API_PORT = 8080;
 const GET_LOBBIES_URL = "/lobbies/"
 const CREATE_LOBBY_URL = GET_LOBBIES_URL + "/create"
+const LOBBY_CODE_COOKIE = "lobby";
+const LOBBY_PAGE_NAME = "/lobby.html"
 
 $(document).ready(function() {
     initButtonJoinHandler();
     initButtonCreateHandler();
+    initNewLobby();
 });
 
 function initButtonJoinHandler() {
@@ -15,16 +18,7 @@ function initButtonJoinHandler() {
         return;
     }
     btnJoin.submit(event => {
-        var url = API_PROTOCOL + API_URL + ":" + API_PORT + GET_LOBBIES_URL;
-        $.getJSON(url, function(data) {
-            found = false;
-            $.each(data, function(key, val) {
-                if (val.name == $('#lobbyCode').val()) {
-                    found = true;
-                }
-            });
-            found ? console.log("Found") : console.log("Not found");
-        });
+        joinLobby($('#lobbyCode').val());
 
         // Stop redirection
         event.preventDefault();
@@ -37,13 +31,87 @@ function initButtonCreateHandler() {
         return;
     }
     buttonCreate.click(function(event) {
-        var url = API_PROTOCOL + API_URL + ":" + API_PORT + CREATE_LOBBY_URL;
+        createLobby();
+        event.preventDefault();
+    });
+}
 
-        $.getJSON(url, function(data) {
+function initNewLobby() {
+    var lobbyCode = getCookie(LOBBY_CODE_COOKIE);
+    if (isNull(lobbyCode) || lobbyCode === "") {
+        return;
+    }
+    var lobbyCodeEle = $("#lobbyCode");
+    if (isNull(lobbyCodeEle)) {
+        return;
+    }
+    lobbyCodeEle.text(lobbyCode);
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(name, value, expirationDate, page) {
+    var cookieValue = getCookie(name);
+    if (!isNull(cookieValue) && cookieValue !== "") {
+        document.cookie = name + "=;  expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    }
+    cookieValue = name + "=" + value;
+    if (!isNull(expirationDate)) {
+        cookieValue += "; expires=" + expirationDate.toUTCString();
+    }
+    if (!isNull(page)) {
+        cookieValue += "; path=" + page;
+    }
+    document.cookie = cookieValue;
+}
+
+function joinLobby(lobbyCode) {
+    var url = API_PROTOCOL + API_URL + ":" + API_PORT + GET_LOBBIES_URL;
+    $.getJSON({
+        url: url,
+        success: function(data) {
+            var lobbyExists = false;
+            $.each(data, function(key, val) {
+                if (val.name == lobbyCode) {
+                    lobbyExists = true;
+                }
+            });
+
+            if (lobbyExists) {
+                var expirationDate = null;
+                setCookie(LOBBY_CODE_COOKIE, lobbyCode, expirationDate, LOBBY_PAGE_NAME);
+                window.location.replace("lobby.html")
+            } else {
+
+            }
+        }
+    });
+}
+
+function createLobby() {
+    var url = API_PROTOCOL + API_URL + ":" + API_PORT + CREATE_LOBBY_URL;
+    $.getJSON({
+        url: url,
+        async: false,
+        success: function(data) {
             found = false;
             console.log("Room code is: " + data.name);
-        });
-        event.preventDefault();
+            document.cookie = LOBBY_CODE_COOKIE + "=" + data.name;
+        }
     });
 }
 
