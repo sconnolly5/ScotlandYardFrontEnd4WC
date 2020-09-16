2cd0a78dc05ea5e64192c0b41ce6c68be088a549
Sam Connolly School of Physical Sciences and Computing <SConnolly5@uclan.ac.uk>
2020-09-17 00:20:09 +0100

    Changed lobby join mechanism.
    Now attempts to join the lobby through a cached session variable if the lobby is still active.

--- js/lobbies.js
+++ js/lobbies.js
@@ -3,22 +3,61 @@ const API_URL = "localhost";
 const API_PORT = 8080;
 const GET_LOBBIES_URL = "/lobbies/"
 const CREATE_LOBBY_URL = GET_LOBBIES_URL + "/create"
-const LOBBY_CODE_COOKIE = "lobby";
-const LOBBY_PAGE_NAME = "/lobby.html"
+const LOBBY_CODE_VAR = "lobby";
+const LOBBY_PAGE = "lobby.html";
+const JOIN_LOBBY_PAGE = "joinLobby.html";
 
 $(document).ready(function() {
+    initNavbarJoinButton();
     initButtonJoinHandler();
     initButtonCreateHandler();
     initNewLobby();
 });
 
+function initNavbarJoinButton() {
+    // Set up handlers for the join lobby button in the navbar
+    var btnNavbarJoinLobby = $('#btnJoinLobby');
+    btnNavbarJoinLobby.click(function(event) {
+        var url = API_PROTOCOL + API_URL + ":" + API_PORT + GET_LOBBIES_URL;
+        $.getJSON({
+            url: url,
+            success: function(data) {
+                var lobbyCode = sessionStorage.getItem(LOBBY_CODE_VAR);
+                var exists = lobbyExists(data, lobbyCode);
+
+                if (exists) {
+                    window.location.replace(LOBBY_PAGE)
+                } else {
+                    sessionStorage.removeItem(LOBBY_PAGE);
+                    window.location.replace(JOIN_LOBBY_PAGE);
+                }
+            }
+        });
+        event.preventDefault();
+    });
+}
+
 function initButtonJoinHandler() {
+    // Set up handlers for the submit join button (on the join lobby page)
     var btnJoin = $('#joinLobbyForm');
     if (isNull(btnJoin)) {
         return;
     }
     btnJoin.submit(event => {
-        joinLobby($('#lobbyCode').val());
+        var lobbyCode = $('#lobbyCode').val();
+        var url = API_PROTOCOL + API_URL + ":" + API_PORT + GET_LOBBIES_URL;
+        $.getJSON({
+            url: url,
+            success: function(data) {
+                var exists = lobbyExists(data, lobbyCode);
+                if (exists) {
+                    sessionStorage.setItem(LOBBY_CODE_VAR, lobbyCode);
+                    window.location.replace(LOBBY_PAGE)
+                } else {
+
+                }
+            }
+        });
 
         // Stop redirection
         event.preventDefault();
@@ -31,13 +70,22 @@ function initButtonCreateHandler() {
         return;
     }
     buttonCreate.click(function(event) {
-        createLobby();
+        var url = API_PROTOCOL + API_URL + ":" + API_PORT + CREATE_LOBBY_URL;
+        $.getJSON({
+            url: url,
+            async: false,
+            success: function(data) {
+                found = false;
+                sessionStorage.setItem(LOBBY_CODE_VAR, data.name);
+                window.location.replace(LOBBY_PAGE)
+            }
+        });
         event.preventDefault();
     });
 }
 
 function initNewLobby() {
-    var lobbyCode = getCookie(LOBBY_CODE_COOKIE);
+    var lobbyCode = sessionStorage.getItem(LOBBY_CODE_VAR);
     if (isNull(lobbyCode) || lobbyCode === "") {
         return;
     }
@@ -48,71 +96,14 @@ function initNewLobby() {
     lobbyCodeEle.text(lobbyCode);
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function setCookie(name, value, expirationDate, page) {
-    var cookieValue = getCookie(name);
-    if (!isNull(cookieValue) && cookieValue !== "") {
-        document.cookie = name + "=;  expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    }
-    cookieValue = name + "=" + value;
-    if (!isNull(expirationDate)) {
-        cookieValue += "; expires=" + expirationDate.toUTCString();
-    }
-    if (!isNull(page)) {
-        cookieValue += "; path=" + page;
-    }
-    document.cookie = cookieValue;
-}
-
-function joinLobby(lobbyCode) {
-    var url = API_PROTOCOL + API_URL + ":" + API_PORT + GET_LOBBIES_URL;
-    $.getJSON({
-        url: url,
-        success: function(data) {
-            var lobbyExists = false;
-            $.each(data, function(key, val) {
-                if (val.name == lobbyCode) {
-                    lobbyExists = true;
-                }
-            });
-
-            if (lobbyExists) {
-                var expirationDate = null;
-                setCookie(LOBBY_CODE_COOKIE, lobbyCode, expirationDate, LOBBY_PAGE_NAME);
-                window.location.replace("lobby.html")
-            } else {
-
-            }
+function lobbyExists(jsonData, lobbyCode) {
+    var lobbyExists = false;
+    $.each(jsonData, function(key, val) {
+        if (val.name == lobbyCode) {
+            lobbyExists = true;
         }
     });
-}
-
-function createLobby() {
-    var url = API_PROTOCOL + API_URL + ":" + API_PORT + CREATE_LOBBY_URL;
-    $.getJSON({
-        url: url,
-        async: false,
-        success: function(data) {
-            found = false;
-            console.log("Room code is: " + data.name);
-            document.cookie = LOBBY_CODE_COOKIE + "=" + data.name;
-        }
-    });
+    return lobbyExists;
 }
 
 function isNull(obj) {
